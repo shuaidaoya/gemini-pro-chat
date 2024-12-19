855f3aae01a29cc4743823bf8aa578b4f1073598
shuaishuai <shuaidaoya@gmail.com>
2024-12-19 23:53:59 +0800

    更新 Generator.tsx
    发送按钮翻译

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -272,7 +272,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} gen-slate-btn>
-            Send
+            发送
           </button>
           <button title="Clear" onClick={clear} gen-slate-btn>
             <IconClear />
